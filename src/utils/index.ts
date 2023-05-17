1eb55f3e3e29acd8750985ec07ff82782e9fb660
letrunks <Trungthekop123@gmail.com>
2023-05-17 10:37:18 +0700

    add file

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export function total(a:number, b:number) {
+  return a+b
+}
+total(5,8)
+const age: number = 20
+const name:string = 'Trunks'
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+colors: {
+  // Màu trên thành naviation, text chỗ feature trang home ..v
+  primary : "#032541";
+  // Màu trên thanh % khi điểm cao
+  secondary :"#39dcaf";
+  // Màu trên thanh % khi điểm trung bình
+  medium:"#8a9028";
+  // 
+  info:"#01b4e4";
+  //Text chữ nhỏ : ngày tháng năm chỗ movie item
+  textMeta:"#666666";
+  light :"#ffffff";
+  dark:"#081c22";
+  // color khi check icon phần movie detail
+  red:"#cf3131"
+  gray:"#dbdbdb"
+  graySoft:"#ececec";
+  grayBold :"#ced3db";
+
+}
